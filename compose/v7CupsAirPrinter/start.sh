dada8d10fa0c5cb3856edb544078737acf8ce7a9
Arthur <584831408@qq.com>
2024-12-31 16:03:24 +0800

    修改打印机设备获取路径

--- compose/v7CupsAirPrinter/start.sh
+++ compose/v7CupsAirPrinter/start.sh
@@ -5,7 +5,7 @@ docker run \
        --net=host \
        -v /var/run/dbus:/var/run/dbus \
        --device /dev/bus \
-       --device /dev/usb \
+       --device /dev/bus/usb \
        -e CUPSADMIN="admin" \
        -e CUPSPASSWORD="admin" \
        tigerj/cups-airprint
